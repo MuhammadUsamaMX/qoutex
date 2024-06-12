b578758dcdea8a7580b52833d5e88cb9c261a688
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2024-06-12 14:15:48 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ docker run -d \
   -v portainer_agent_data:/data \
   --restart always \
   -e EDGE=1 \
-  -e EDGE_ID=0353397f-6fb7-458d-855a-2741952f1686 \
-  -e EDGE_KEY=aHR0cHM6Ly9wb3J0YWluZXIuc2VydmVyLmFtdWl6LmNvbS98MTQxLjE0Ny43My41Nzo4MDAwfERFWnVyY3dLWWZTUFlBa3pzNGRkZ1hwaDg2QlhYM0g4Vk9FdDg0K2NJQ3c9fDM \
+  -e EDGE_ID=5f8fe041-b9c0-4d93-9453-45c78320cd25 \
+  -e EDGE_KEY=aHR0cHM6Ly9wb3J0YWluZXIuc2VydmVyLmFtdWl6LmNvbXxwb3J0YWluZXIuc2VydmVyLmFtdWl6LmNvbTo4MDAwfERFWnVyY3dLWWZTUFlBa3pzNGRkZ1hwaDg2QlhYM0g4Vk9FdDg0K2NJQ3c9fDQ \
   -e EDGE_INSECURE_POLL=1 \
+  -e EDGE_ASYNC=1 \
   --name portainer_edge_agent \
   portainer/agent:2.19.5
   
