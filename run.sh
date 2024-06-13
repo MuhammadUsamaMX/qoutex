7d23a3a79bdd4b60db06e9cfce379682815381bb
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2024-06-13 20:58:16 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
-docker run -d \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /var/lib/docker/volumes:/var/lib/docker/volumes \
-  -v /:/host \
-  -v portainer_agent_data:/data \
-  --restart always \
-  -e EDGE=1 \
-  -e EDGE_ID=67216eaf-bbbc-4052-8bde-ba6ee903e146 \
-  -e EDGE_KEY=aHR0cHM6Ly9wb3J0YWluZXIuc2VydmVyLmFtdWl6LmNvbXxwb3J0YWluZXIuc2VydmVyLmFtdWl6LmNvbTo4MDAwfERFWnVyY3dLWWZTUFlBa3pzNGRkZ1hwaDg2QlhYM0g4Vk9FdDg0K2NJQ3c9fDU \
-  -e EDGE_INSECURE_POLL=1 \
-  --name portainer_edge_agent_2 \
-  portainer/agent:2.19.5
-  
+
+[Interface]
+PrivateKey = yFZr4ZkHgwa6rfAm8tLHDxOFLhzXvhWuKoMarEX94mU=
+Address = 10.8.0.4/24
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = OaOQoqJ+uL+JSl0JBTyO6iUsx6FiZ9TBxgdGW4Hdfn4=
+PresharedKey = ZNLTeJlnkz/S4tAlmgu60p2YTJxQExdP3to+2GtKM0E=
+AllowedIPs = 10.8.0.1/32
+PersistentKeepalive = 25
+Endpoint = 141.147.73.57:51820
