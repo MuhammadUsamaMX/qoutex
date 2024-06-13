f5b53eee95b4add6cfc4608aa12707c6540b914d
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2024-06-13 20:59:19 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,1 @@
-
-[Interface]
-PrivateKey = yFZr4ZkHgwa6rfAm8tLHDxOFLhzXvhWuKoMarEX94mU=
-Address = 10.8.0.4/24
-DNS = 1.1.1.1
-
-[Peer]
-PublicKey = OaOQoqJ+uL+JSl0JBTyO6iUsx6FiZ9TBxgdGW4Hdfn4=
-PresharedKey = ZNLTeJlnkz/S4tAlmgu60p2YTJxQExdP3to+2GtKM0E=
-AllowedIPs = 10.8.0.1/32
-PersistentKeepalive = 25
-Endpoint = 141.147.73.57:51820
+asdcu
