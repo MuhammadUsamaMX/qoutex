9b9e4c7e5f0694caf32c1b50afd3f9ca58d5c275
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2024-07-26 17:03:02 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
-
-sudo dpkg -i cloudflared.deb && 
-
-sudo cloudflared service install eyJhIjoiMTUzYWMyZjczZjNhMGM3NjJjZjE1MThhYzBlZDM1MTEiLCJ0IjoiYmQ5NWJkNDEtNTc2My00N2U4LTgwYTYtNDU1NWNmODY5ZDM3IiwicyI6Ik1qRTNaR1ZtWW1JdFltRmpOQzAwTXpZeUxUaG1PR1F0Tmpnek5XVmtNVE5pTUdRNCJ9
+URL=https://www.aapanel.com/script/install_7.0_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO "$URL" ;else wget --no-check-certificate -O install_7.0_en.sh "$URL";fi;bash install_7.0_en.sh aapanel
