f1c0bb6ab04f1469eb0264132734d9131577d55f
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2024-07-27 20:27:38 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-URL=https://www.aapanel.com/script/install_7.0_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO "$URL" ;else wget --no-check-certificate -O install_7.0_en.sh "$URL";fi;bash install_7.0_en.sh aapanel
+sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh)
