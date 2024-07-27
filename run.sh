a54fad272b90e3b6975127ea2fa77e277bc97cb4
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2024-07-27 20:55:09 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh)
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiMTUzYWMyZjczZjNhMGM3NjJjZjE1MThhYzBlZDM1MTEiLCJ0IjoiYmQ5NWJkNDEtNTc2My00N2U4LTgwYTYtNDU1NWNmODY5ZDM3IiwicyI6Ik1qRTNaR1ZtWW1JdFltRmpOQzAwTXpZeUxUaG1PR1F0Tmpnek5XVmtNVE5pTUdRNCJ9
